f7445fecdd94af85f13c4d915c469aa78d327faf
emptyhead <921310+emptyhead@users.noreply.github.com>
2026-02-11 16:01:56 +0000

    feat: add placeholder graphics system with unicorn and fairy sprites

--- src/entities/Unicorn.js
+++ src/entities/Unicorn.js
@@ -1,4 +1,6 @@
 // src/entities/Unicorn.js
+import { generateUnicornTexture } from '../utils/PlaceholderGraphics.js';
+
 export class Unicorn {
     constructor(config) {
         this.id = config.id || Date.now();
@@ -16,6 +18,9 @@ export class Unicorn {
 
         this.growthProgress = config.growthProgress || 0;
         this.isUnhappy = false;
+        
+        // Phaser sprite reference (set when added to scene)
+        this.sprite = null;
     }
 
     getEXP() {
@@ -23,22 +28,31 @@ export class Unicorn {
         return values[this.stage];
     }
 
+    /**
+     * Create a sprite for this unicorn in the given scene
+     * @param {Phaser.Scene} scene - The scene to add the sprite to
+     * @param {number} x - X position
+     * @param {number} y - Y position
+     * @returns {Phaser.GameObjects.Sprite} The created sprite
+     */
+    createSprite(scene, x, y) {
+        const textureKey = generateUnicornTexture(scene, this.type, this.stage);
+        this.sprite = scene.add.sprite(x, y, textureKey);
+        return this.sprite;
+    }
+
+    /**
+     * Update the sprite texture (e.g., after stage change)
+     * @param {Phaser.Scene} scene - The scene containing the sprite
+     */
+    updateSpriteTexture(scene) {
+        if (this.sprite) {
+            const textureKey = generateUnicornTexture(scene, this.type, this.stage);
+            this.sprite.setTexture(textureKey);
+        }
+    }
+
     update(delta) {
         // Handle stat decay and unhappy state checks here
-    }
-}
-
-// src/entities/Fairy.js
-export class Fairy {
-    constructor(id, isMain = false) {
-        this.id = id;
-        this.isMain = isMain;
-        this.learnedActions = {
-            feed: isMain, // Main fairy knows all
-            love: isMain,
-            play: isMain,
-            sleep: isMain
-        };
-        this.cooldown = 0;
     }
 }
